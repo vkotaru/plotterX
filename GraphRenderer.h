8340b119c7974a30dc3c507f237cd36b718f4869
ashishbajaj91 <mm10b009@smail.iitm.ac.in>
2016-12-05 00:29:14 -0500

    Fixing the axis bug in the UI branch
    Fixing the axis bug in the UI branch

--- GraphRenderer.h
+++ GraphRenderer.h
@@ -87,13 +87,15 @@ class GraphRenderer
 		glEnable(GL_LINE_STIPPLE);
 		glLineStipple(1, 0xf0f0);
 
+		double deltaX = (Xmax - Xmin) / NoOfDivisions;
+		double deltaY = (Ymax - Ymin) / NoOfDivisions;
+		glBegin(GL_LINES);
 		for (int i = 1; i < NoOfDivisions; i++)
 		{
-			glBegin(GL_LINES);
-			DrawFig.DrawLines3D(Xmin, Ymax*i / NoOfDivisions, Zmin, Xmax, Ymax*i / NoOfDivisions, Zmin, GL_LINES);
-			DrawFig.DrawLines3D(Xmax*i / NoOfDivisions, Ymin, Zmin, Xmax*i / NoOfDivisions, Ymax, Zmin, GL_LINES);
-			glEnd();
+			DrawFig.DrawLines3D(Xmin, Ymin+i*deltaY, Zmin, Xmax, Ymin+i*deltaY, Zmin, GL_LINES);
+			DrawFig.DrawLines3D(Xmin+i*deltaX, Ymin, Zmin, Xmin+i*deltaX, Ymax, Zmin, GL_LINES);
 		}
+		glEnd();
 		glDisable(GL_LINE_STIPPLE);
 	};
 
@@ -131,16 +133,20 @@ class GraphRenderer
 		DrawFig.WriteMsgAtXYZ(Xmin, Ymin, Zmax, CoordinateMarkings);
 
 
+		double deltaX = (Xmax - Xmin) / NoOfDivisions;
+		double deltaY = (Ymax - Ymin) / NoOfDivisions;
+		double deltaZ = (Zmax - Zmin) / NoOfDivisions;
+
 		for (int i = 1; i < NoOfDivisions; i++)
 		{
-			sprintf(CoordinateMarkings, "%lf \0", Xmax*i / NoOfDivisions);
-			DrawFig.WriteMsgAtXYZ(Xmax*i / NoOfDivisions, Ymin, Zmin, CoordinateMarkings);
+			sprintf(CoordinateMarkings, "%lf \0", Xmin + i*deltaX);
+			DrawFig.WriteMsgAtXYZ(Xmin + i*deltaX, Ymin, Zmin, CoordinateMarkings);
 
-			sprintf(CoordinateMarkings, "%lf \0", Ymax*i / NoOfDivisions);
-			DrawFig.WriteMsgAtXYZ(Xmin, Ymax*i / NoOfDivisions, Zmin, CoordinateMarkings);
+			sprintf(CoordinateMarkings, "%lf \0", Ymin + i*deltaY);
+			DrawFig.WriteMsgAtXYZ(Xmin, Ymin + i*deltaY, Zmin, CoordinateMarkings);
 
-			sprintf(CoordinateMarkings, "%lf \0", Zmax*i / NoOfDivisions);
-			DrawFig.WriteMsgAtXYZ(Xmin, Ymin, Zmax*i / NoOfDivisions, CoordinateMarkings);
+			sprintf(CoordinateMarkings, "%lf \0", Zmin + i*deltaZ);
+			DrawFig.WriteMsgAtXYZ(Xmin, Ymin, Zmin + i*deltaZ, CoordinateMarkings);
 		}
 
 		if (DrawMesh)
